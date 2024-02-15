b6b6593c59d6cb05fedde5d6bfc9ab8e7687182a
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-02-15 18:23:15 +0100

    Create Move struct

---
+++ src/move.cpp
@@ -0,0 +1,4 @@
+#include "move.h"
+
+Move::Move(unsigned int row, unsigned int col)
+    : row(row), col(col) {}
---
+++ src/move.h
@@ -0,0 +1,11 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+struct Move
+{
+    Move(unsigned int row, unsigned int col);
+
+    unsigned int row, col;
+};
+
+#endif
