5c7e7eee875b74c7c187f4d75bce6aabfe143a4c
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-02-15 20:03:21 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-#include "board.h"
 #include "human_player.h"
 #include "mark.h"
+#include "game.h"
 
 int main()
 {
     std::cout << "tic-tac-toe\n";
     Player *player_x = new HumanPlayer(Mark::x);
-    Board *board = new Board();
-    player_x->make_move(board);
-    std::cout << *board << '\n';
+    Player *player_o = new HumanPlayer(Mark::o);
+    Game *game = new Game(player_x, player_o);
+    game->start();
     return 0;
 }
