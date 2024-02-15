0f9e446c89e5ef9ebd2a5ce3169d6371c814f7e3
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-02-15 19:16:44 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 
-#include "board.h"
-#include "player.h"
-
 int main()
 {
     std::cout << "tic-tac-toe\n";
-    Player *player_1 = new Player('x');
-    Player *player_2 = new Player('o');
-    Board *board = new Board();
-    board->make_move(Move(0, 1), player_1);
-    std::cout << *board << '\n';
-    delete player_1, player_2, board;
     return 0;
 }
