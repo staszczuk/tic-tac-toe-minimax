206a9ec8c142149287d5664805d29b499638e7c3
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-02-15 19:26:41 +0100

    Add << operator overload

--- src/move.cpp
+++ src/move.cpp
@@ -2,3 +2,8 @@
 
 Move::Move(unsigned int row, unsigned int col)
     : row(row), col(col) {}
+
+std::ostream &operator<<(std::ostream &os, const Move &move)
+{
+    os << '[' << move.row << ' ' << move.col << ']';
+}
--- src/move.h
+++ src/move.h
@@ -1,9 +1,13 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+#include <iostream>
+
 struct Move
 {
     Move(unsigned int row, unsigned int col);
+
+    friend std::ostream &operator<<(std::ostream &os, const Move &move);
 
     unsigned int row, col;
 };
