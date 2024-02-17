97f45f3c7e909e798b09132523f8f78073462c5f
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-02-17 02:44:04 +0100

    Change methods definitions order

--- src/player.cpp
+++ src/player.cpp
@@ -7,13 +7,13 @@ Mark Player::get_mark() const
     return this->mark;
 }
 
+void Player::set_mark(Mark mark)
+{
+    this->mark = mark;
+}
+
 std::ostream &operator<<(std::ostream &os, const Player &player)
 {
     os << player.mark;
     return os;
-}
-
-void Player::set_mark(Mark mark)
-{
-    this->mark = mark;
 }
