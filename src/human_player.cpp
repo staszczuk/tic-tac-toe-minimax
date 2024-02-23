c27410e1c1ae2cc6f41e04d55e7b168edd0278fe
Paweł Staszczuk <41273017+staszczuk@users.noreply.github.com>
2024-02-23 19:03:46 +0100

    Update make_move method

--- src/human_player.cpp
+++ src/human_player.cpp
@@ -4,22 +4,17 @@ HumanPlayer::HumanPlayer() {}
 
 void HumanPlayer::make_move(Board *board) const
 {
-    std::vector<Move> available_moves = board->get_available_moves();
-    std::cout << "Choose move:\n";
-    for (unsigned int i = 0; i < available_moves.size(); i++)
+    unsigned int row, col;
+    std::cout << "your move [row col]: ";
+    std::cin >> row >> col;
+    for (Move move : board->get_available_moves())
     {
-        std::cout << i << ' ' << available_moves[i];
-        if (i + 1 != available_moves.size())
+        if (move.row == row and move.col == col)
         {
-            std::cout << ", ";
-        }
-        else
-        {
-            std::cout << '\n';
+            board->make_move(move, this->mark);
+            return;
         }
     }
-    std::cout << "Choice (0 - " << available_moves.size() << "): ";
-    unsigned int move_index;
-    std::cin >> move_index;
-    board->make_move(available_moves.at(move_index), this->mark);
+    std::cout << "invalid move. try again...\n";
+    this->make_move(board);
 }
